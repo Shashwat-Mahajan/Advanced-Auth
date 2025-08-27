4b2962943addd26caa525a1cd19c0de69981e892
Shashwat Mahajan <shashwatmahajan2@gmail.com>
2025-08-27 23:59:39 +0530

    Working on the Password Strength Meter

---
+++ Frontend/src/components/PasswordStrengthMeter.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const PasswordCriteria = ({password}) => {
+    const criteria = [
+      { label: "At least 6 characters", met: password.length >= 6 },
+      { label: "Contains uppercase letter", met: /[A-Z]/.test(password) },
+      { label: "Contains lowercase letter", met: /[a-z]/.test(password) },
+      { label: "Contains a number", met: /\d/.test(password) },
+      {
+        label: "Contains special character",
+        met: /[^A-Za-z0-9]/.test(password),
+      },
+    ];
+}
+
+
+const PasswordStrengthMeter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PasswordStrengthMeter
